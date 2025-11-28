03ae129aa022a98cfdf785233b5c5997fdac0d82
mohammadsadranoroozi <162976370+mohammadsadranoroozi@users.noreply.github.com>
2025-11-28 15:40:10 +0330

    Update student.py

--- student.py
+++ student.py
@@ -1,4 +1,6 @@
 class Student:
     def __init__(self, name):
         self.name = name
+    def get_info(self):
+        return f"Student name is {self.name}"
 my_student = Student("Mohammadsadra Noroozi")
